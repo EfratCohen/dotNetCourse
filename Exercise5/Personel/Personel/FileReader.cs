fc232c51a23b1ac344b5d4cc8970fa7f96b47389
Efrat <efratnor@gmail.com>
2016-07-11 23:40:11 +0300

    lab 10.1

---
+++ Exercise5/Personel/Personel/FileReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personel
+{
+    class FileReader
+    {
+        /// Write a method that reads all data from the file(you can use a StreamReader)
+        /// and places it in a List of strings.
+        public List<string> ReadsAllData(FileStream stm)
+        {
+            var fileData = new List<string>();
+            string dataLine = "";
+            if (stm != null)
+            {
+                StreamReader reader = new StreamReader(stm);
+
+                dataLine= reader.ReadLine();
+                while (dataLine != null)
+                {
+                    fileData.Add(dataLine);
+                    dataLine = reader.ReadLine();
+
+                }
+                reader.Close();
+            }
+            else
+            {
+                throw new ArgumentNullException("stm", "ReadsAllData needs a valid FileStream to read from at invokation");
+            }
+            
+            return fileData;
+        }
+ 
+    }
+}
--- Exercise5/Personel/Personel/Program.cs
+++ Exercise5/Personel/Personel/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace Personel
     {
         static void Main(string[] args)
         {
+           
+            using (FileStream stm = new FileStream("names.txt", FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                Console.WriteLine("Printing the whole names.txt file we prepered for ReadsAllData use:");
+                var reader = new FileReader();
+                var stringsToPrint = reader.ReadsAllData(stm);
+                foreach (var str in stringsToPrint)
+                {
+                    Console.WriteLine(str);
+
+                }
+            }
         }
     }
 }
