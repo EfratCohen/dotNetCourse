579c89c23debf3dde06e45f43f309496c663427a
Efrat <efratnor@gmail.com>
2016-07-12 14:30:47 +0300

    almost finished, I hope. must check it to ensure

--- Exercise5/FileFinder/FileFinder/FileFinder.cs
+++ Exercise5/FileFinder/FileFinder/FileFinder.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System;using System.Collections.Generic;
+
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,10 +9,25 @@ namespace FileFinder
 {
     class FileFinder
     {
-        public List<string> SearchForAllFilesContains(string path, string pattren)
+        public void  SearchForAllFilesContains(string path, string pattren, List<FileInfo> filesContainList)
         {
-            var filesNames = new List<string>();
-            return filesNames;
+            
+            var recentDirectoryInf = new DirectoryInfo(path);
+            var files = recentDirectoryInf.GetFiles();
+            foreach (var file in files)
+                if (File.ReadAllText(file.FullName).Contains(pattren))
+                {
+                    filesContainList.Add(file);
+                }
+            //Scan recursively
+            DirectoryInfo[] currentSubDirectories = recentDirectoryInf.GetDirectories();
+            if (currentSubDirectories == null || currentSubDirectories.Length < 1)
+            {
+                return;
+            }   
+            foreach (DirectoryInfo subDirectoryInf in currentSubDirectories)
+                SearchForAllFilesContains(subDirectoryInf.FullName, pattren, filesContainList);
         }
     }
 }
+
--- Exercise5/FileFinder/FileFinder/Program.cs
+++ Exercise5/FileFinder/FileFinder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +9,25 @@ namespace FileFinder
 {
     class Program
     {
-        // Use the first parameter from the command line as a directory path, and search for all files that contain a string passed as the second parameter to the command line. 
+        // Use the first parameter from the command  as a directory path, and search for all files that contain a string passed as the second parameter to the command line. 
         static void Main(string[] args)
         {
             if (args.Length >= 2)
             {
                 string path = args[0];
                 string patteren = args[1];
+                var filesToDisplay = new List<FileInfo>();
                 var finder = new FileFinder();
-                var fileNamesToPrint = finder.SearchForAllFilesContains(path, patteren);
-                foreach (var fileName in fileNamesToPrint)
+                finder.SearchForAllFilesContains(path, patteren, filesToDisplay);
+                Console.WriteLine($"we have found {filesToDisplay.Count} files which contains the string {patteren}");
+                foreach (var file in filesToDisplay)
                 {
-                    Console.WriteLine(fileName);
-
+                    Console.WriteLine($"{file.Name} with size (in bytes): {file.Length}");
                 }
             }
             else
             {
-                throw new ArgumentException("args", "the program must have tow paremeters from the commend ");
+                throw new ArgumentException("args", "the program must have two paremeters from the commend line");
             }
 
         }
