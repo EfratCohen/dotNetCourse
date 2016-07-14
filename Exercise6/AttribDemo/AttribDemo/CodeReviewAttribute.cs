e319349cc15e72d785a1d93e9c5338fd931de294
Efrat <efratnor@gmail.com>
2016-07-14 12:15:42 +0300

    with some properties

---
+++ Exercise6/AttribDemo/AttribDemo/CodeReviewAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttribDemo
+{
+    class CodeReviewAttribute
+    {
+        //class should contain 3 properties:
+        //the reviewer name, the review date and whether the code is approved.
+        // Make sure they are also accessible as standalone properties. 
+       public string ReviewerName { get ; set; }
+       public date ReviewDate { get; set; }
+       public bool IsTheCodeApproved { get; set; }
+        //Add a constructor that accepts all 3 properties
+        public CodeReviewAttribute(string name,date day,bool approved)
+        {
+            ReviewerName = name;
+            ReviewDate = day;
+            IsTheCodeApproved = approved;
+        }
+        ///Add the AttributeUsage attribute to the class 
+        ///to allow the attribute on classes and structs only 
+        ///(multiple usages are allowed).
+
+
+    }
+    struct date
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public date(int day,int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+    }
+}
