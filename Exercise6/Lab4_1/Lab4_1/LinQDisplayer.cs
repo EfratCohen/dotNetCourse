5cf54727fa0d9b871dfaf149a71a7b31fa3ba36a
Efrat <efratnor@gmail.com>
2016-07-22 10:48:02 +0300

    finished a taak

---
+++ Exercise6/Lab4_1/Lab4_1/LinQDisplayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1
+{
+    /// <summary>
+    /// 1
+    ///  Create queries and display results for the following:
+    ///  
+    ///  b. Display all processes running on your system (process name, id and start time), whose thread count is less than 5. 
+    ///  Sort by process id. 
+    ///  c. (*) Extend (b) by grouping the processes by their base priority. 
+    ///  d. Display the total number of threads in the system. 
+    ///  2.
+    ///  (*) Create an extension method that extends object with a method called CopyTo that copies all public properties from this object to another object passed as argument. Make sure only writable properties are written and readable properties are read. Use LINQ. 
+    /// </summary>
+    /// <param name="args"></param>
+    ///
+    class LinQDisplayer
+    {
+        /// <summary>
+        ///  return all the public interface names in the mscorlib assembly and the number of methods in each type. 
+        ///  Sort by type name. for their Display in the main.
+        /// </summary>
+        public IEnumerable<Type> GetPublicInterfaceName()
+        {
+
+            var mscrlibAssembly = typeof(string).Assembly;
+                foreach (Type ti in mscrlibAssembly.GetTypes().Where(x => (x.IsInterface&&x.IsPublic)).OrderBy(o=>o.Name))
+                {
+                    yield return ti; 
+                }
+            }
+        }
+        /// <summary>
+        ///  Display all processes running on your system (process name, id and start time), whose thread count is less than 5. 
+        ///  Sort by process id.
+        /// </summary>
+        //public void RunningProcess()
+        //{ }
+}
+
---
+++ Exercise6/Lab4_1/Lab4_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1
+{
+    class Program
+    {
+        /// <summary>
+        /// 1
+        ///  Create queries and display results for the following:
+        ///  a. Display all the public interface names in the mscorlib assembly and the number of methods in each type. 
+        ///  Sort by type name. 
+        ///  b. Display all processes running on your system (process name, id and start time), whose thread count is less than 5. 
+        ///  Sort by process id. 
+        ///  c. (*) Extend (b) by grouping the processes by their base priority. 
+        ///  d. Display the total number of threads in the system. 
+        ///  2.
+        ///  (*) Create an extension method that extends object with a method called CopyTo that copies all public properties from this object to another object passed as argument. Make sure only writable properties are written and readable properties are read. Use LINQ. 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var myDisplayer = new LinQDisplayer();
+            var myItrerator =  myDisplayer.GetPublicInterfaceName();
+            foreach (var item in myItrerator)
+            {
+                Console.WriteLine(item);
+            } 
+             
+        }
+    }
+}
