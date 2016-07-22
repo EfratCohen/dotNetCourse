544a033506deeb9eeb804a15f4143ca2b3a050ec
aa <aaa>
2016-07-22 13:09:50 +0300

    aaaa

---
+++ Exercise6/Lab4_1/Lab4_1/Checker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1
+{
+    class Checker
+    {
+        public DateTime StartTimeReturnIfCan(Process p)
+        {
+            try
+            {
+                return p.StartTime;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("we get {0,5} exeption while the process StartTime get ", ex.GetType());
+                return new DateTime();
+            }
+        }
+    }
+}
--- Exercise6/Lab4_1/Lab4_1/LinQDisplayer.cs
+++ Exercise6/Lab4_1/Lab4_1/LinQDisplayer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -9,15 +10,13 @@ using System.Threading.Tasks;
 namespace Lab4_1
 {
     /// <summary>
-    /// 1
-    ///  Create queries and display results for the following:
-    ///  
+    /// Create queries and display results for the following:
+    ///  a. Display all the public interface names in the mscorlib assembly and the number of methods in each type. 
+    ///  Sort by type name. 
     ///  b. Display all processes running on your system (process name, id and start time), whose thread count is less than 5. 
     ///  Sort by process id. 
     ///  c. (*) Extend (b) by grouping the processes by their base priority. 
     ///  d. Display the total number of threads in the system. 
-    ///  2.
-    ///  (*) Create an extension method that extends object with a method called CopyTo that copies all public properties from this object to another object passed as argument. Make sure only writable properties are written and readable properties are read. Use LINQ. 
     /// </summary>
     /// <param name="args"></param>
     ///
@@ -27,18 +26,30 @@ namespace Lab4_1
         ///  return all the public interface names in the mscorlib assembly and the number of methods in each type. 
         ///  Sort by type name. for their Display in the main.
         /// </summary>
-        public IEnumerable<Type> GetPublicInterfaceName()
+        public IEnumerable<Type> PublicInterface()
         {
 
             var mscrlibAssembly = typeof(string).Assembly;
                return (mscrlibAssembly.GetTypes().Where(x => (x.IsInterface&&x.IsPublic)).OrderBy(o=>o.Name));
             }
+        
+         //<summary>
+         // return all processes running on your system (process name, id and start time), whose thread count is less than 5. 
+        // Sort by process id. for their Display in the main.
+         //</summary>
+        public IEnumerable<Process> RunningProcess()
+        {
+           return Process.GetProcesses().Where(p => (p.Threads.Count < 5)).OrderBy(p => p.Id);
         }
-        /// <summary>
-        ///  Display all processes running on your system (process name, id and start time), whose thread count is less than 5. 
-        ///  Sort by process id.
-        /// </summary>
-        //public void RunningProcess()
-        //{ }
+        public IEnumerable<IGrouping<int,Process>> RunningProcessExtend(IEnumerable<Process> procces)
+        {
+           return procces.GroupBy(p => p.BasePriority);
+        }
+        //d.
+        public int TotalSystemThreadsNumber()
+        {
+            return Process.GetProcesses().Select(p =>(p.Threads.Count)).Sum();
+        }
+    }
 }
 
