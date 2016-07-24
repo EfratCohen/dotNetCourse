bd81fd4a3afe813f3b4e36b99805ed2feab13836
Efrat <efratnor@gmail.com>
2016-07-24 23:45:03 +0300

    finished :)

--- Adv2/XLinq/XLinq/Program.cs
+++ Adv2/XLinq/XLinq/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -28,50 +29,45 @@ namespace XLinq
 {
     class Program
     {
-        /// <summary>
-        ///  2. (*) In the Main method, create an XElement object encapsulating the following information: 
-        /// all the classes (not structs or interfaces) in the mscorlib assembly that are public. 
-        /// For each class, add elements for each public instance property with its type and name, 
-        ///     and all public instance methods with their name (not including inherited ones that are not overridden), 
-        ///     return type and parameters(type and name).
-        /// 3. (*) Create the following queries on the resulting XML: 
-        /// a.List all types that have no properties.Order them by name. Also display how many are there. 
-        /// b.Count the total number of methods, not including inherited ones. 
-        /// c.Do some more statistics: how many properties are there? What is the most common type as a parameter?  
-        /// d.Sort the types by the number of methods in descending order. 
-        ///     For each get the number of properties and the number of methods. 
-        ///     Create a new XML for the results.
-        /// e.Group all the types by the number of methods, in descending order based on the number of methods. 
-        ///     Within each group sort by type name in ascending order. 
+
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //2.
-            var mscorlibTypesXml = new XElement("PublicClassesFromMscorlibAssembly", from publicClassType in typeof(string).Assembly.GetTypes()
-                                                                         where publicClassType.IsPublic && publicClassType.IsClass
-                                                                         select new XElement("PublicClass",
-                                          new XAttribute("FullName", publicClassType.FullName),
-                                          new XElement("Properties",
-                                          from publicProperty in publicClassType.GetProperties().Where(propInfo => (propInfo.CanRead))
-                                          select new XElement("Property",
-                                              new XAttribute("Name", publicProperty.Name),
-                                              new XAttribute("Type", publicProperty.PropertyType))),
-                                              new XElement("Methods",
-                                         from InstanceMethod in publicClassType.GetMethods(System.Reflection.BindingFlags.DeclaredOnly)
-                                         select new XElement("Method",
-                                                  new XAttribute("Name", InstanceMethod.Name),
-                                                  new XAttribute("ReturnType", InstanceMethod.ReturnType),
-                                                  new XElement("Parameters",
-                                                  from param in InstanceMethod.GetParameters()
-                                                  select new XElement("Parameter",
-                                                            new XAttribute("Name", param.Name),
-                                                            new XAttribute("Type", param.ParameterType)))))));
-            //3.//a.List all types that have no properties.Order them by name. Also display how many are there.
-            var typesWithNoProperties = from publicClass in mscorlibTypesXml.Descendants("PublicClass")
-                                        where publicClass.Element("Properties").IsEmpty
+            ///2. create an XElement object encapsulating the following information: 
+            /// all the classes (not structs or interfaces) in the mscorlib assembly that are public. 
+            /// For each class, add elements for each public instance property with its type and name, 
+            ///     and all public instance methods with their name (not including inherited ones that are not overridden), 
+            ///     return type and parameters(type and name).
+            var publicClasses = from publicClassType in typeof(string).Assembly.GetExportedTypes()
+                                where publicClassType.IsClass
+                                let pClassProperties = publicClassType.GetProperties()
+                                select new XElement("Type",
+                                    new XAttribute("FullName", publicClassType.FullName),
+                                    new XElement("Properties",
+                                        from property in pClassProperties
+                                        select new XElement("Property",
+                                            new XAttribute("Name", property.Name),
+                                            new XAttribute("Type", property.PropertyType.FullName ?? "T"))),
+                                    new XElement("Methods",
+                                        from method in publicClassType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                                        select new XElement("Method",
+                                            new XAttribute("Name", method.Name),
+                                            new XAttribute("ReturnType", method.ReturnType.FullName ?? "T"),
+                                            new XElement("Parameters",
+                                                from param in method.GetParameters()
+                                                select new XElement("Parameter",
+                                                    new XAttribute("Name", param.Name),
+                                                    new XAttribute("Type", param.ParameterType))))));
+            var mscorlibTypesXml = new XElement("PublicClassesFromMscorlibAssembly", publicClasses);
+            mscorlibTypesXml.Save("file4check.xml");
+            ///3.
+            ///a.List all types that have no properties.Order them by name. Also display how many are there.
+            var typesWithNoProperties = from publicClass in publicClasses
+                                        where publicClass.Element("Properties").Descendants().Count() == 0
                                         orderby (string)publicClass.Attribute("FullName")
-                                        select new {
+                                        select new
+                                        {
                                             FullName = (string)publicClass.Attribute("FullName"),
                                         };
             foreach (var type in typesWithNoProperties)
@@ -79,26 +75,60 @@ namespace XLinq
                 Console.WriteLine(type);
             }
             Console.WriteLine($"there are {typesWithNoProperties.Count()} types with no properties");
-            //b.Count the total number of methods, not including inherited ones. 
-            var methods = from typesMethod in mscorlibTypesXml.Descendants("Methods")
-                                        select new 
-                                        {
-                                           Method = typesMethod.Element("Method")
-                                        };
-            var methodsCount = methods.Count();
+            ///3.
+            ///     b.Count the total number of methods, not including inherited ones. 
+            var methodsCount = publicClasses.Sum(pC => pC.Descendants("Method").Count());
             Console.WriteLine($"there are {methodsCount} methods ");
-            // c.Do some more statistics: how many properties are there? 
-            var properties = from typesProperties in mscorlibTypesXml.Descendants("Properties")
-                          select new
-                          {
-                              property = typesProperties.Element("Property")
-                          };
-            var propertiesCount = properties.Count();
+            ///3.
+            /// c.Do some more statistics: how many properties are there?  
+            var propertiesCount = publicClasses.Sum(pC => pC.Descendants("Property").Count());
             Console.WriteLine($" there are {propertiesCount} properties");
-            //What is the most common type as a parameter ?
-            
-
-
+            ///3.
+            /// c.cont'
+            ///What is the most common type as a parameter ?
+            var parametersTypes = from param in publicClasses.Descendants("Parameter")
+                                  group param by (string)param.Attribute("Type")
+                          into paramsGroup
+                                  orderby paramsGroup.Count() descending
+                                  select new
+                                  {
+                                      Name = paramsGroup.Key,
+                                      Count = paramsGroup.Count()
+                                  };
+            Console.WriteLine($"the most common method parameter type is {parametersTypes.First().Name}");
+            ///3.
+            /// d.Sort the types by the number of methods in descending order. 
+            ///     For each get the number of properties and the number of methods. 
+            ///     Create a new XML for the results.
+            var typesMethodPropNo = from publicClass in publicClasses
+                                    let methodsNo = publicClass.Descendants("Method").Count()
+                                    orderby methodsNo descending
+                                    select new
+                                    {
+                                        TypeName = (string)publicClass.Attribute("FullName"),
+                                        MethodsNo = methodsNo,
+                                        PropertiesNo = publicClass.Descendants("Property").Count()
+                                    };
+            var mscorlibTypesConters = new XElement("typesMethodPropNo", typesMethodPropNo);
+            mscorlibTypesConters.Save("file4check2.xml");
+            ///3.
+            /// e.Group all the types by the number of methods, in descending order based on the number of methods. 
+            ///     Within each group sort by type name in ascending order. 
+            var methodsNoGroupTypes = from publicClass in publicClasses
+                                      let methodsNo = publicClass.Descendants("Method").Count()
+                                      orderby (string)publicClass.Attribute("FullName")
+                                      group new
+                                      {
+                                          Methods = methodsNo,
+                                          Properties = publicClass.Descendants("Property").Count(),
+                                          Name = (string)publicClass.Attribute("FullName")
+                                      } by methodsNo
+                                           into methodsNoGroup
+                                      orderby methodsNoGroup.Key descending
+                                      select methodsNoGroup;
+            var mscorlibTypesGroups = new XElement("methodsNoGroupTypes", methodsNoGroupTypes);
+            mscorlibTypesGroups.Save("file4check3.xml");
         }
     }
 }
+
