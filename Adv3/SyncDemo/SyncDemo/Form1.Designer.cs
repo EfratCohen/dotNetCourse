36a0fa2a79b51f4a17dac2e5b9f749aff819758d
Efrat <efratnor@gmail.com>
2016-07-28 19:25:13 +0300

    start

---
+++ Adv3/SyncDemo/SyncDemo/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SyncDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSTART = new System.Windows.Forms.Button();
+            this.out_listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonSTART
+            // 
+            this.buttonSTART.Location = new System.Drawing.Point(49, 12);
+            this.buttonSTART.Name = "buttonSTART";
+            this.buttonSTART.Size = new System.Drawing.Size(68, 32);
+            this.buttonSTART.TabIndex = 0;
+            this.buttonSTART.Text = "START";
+            this.buttonSTART.UseVisualStyleBackColor = true;
+            this.buttonSTART.Click += new System.EventHandler(this.buttonSTART_Click);
+            // 
+            // out_listBox
+            // 
+            this.out_listBox.FormattingEnabled = true;
+            this.out_listBox.Location = new System.Drawing.Point(49, 68);
+            this.out_listBox.Name = "out_listBox";
+            this.out_listBox.Size = new System.Drawing.Size(178, 212);
+            this.out_listBox.TabIndex = 1;
+            this.out_listBox.SelectedIndexChanged += new System.EventHandler(this.out_listBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 299);
+            this.Controls.Add(this.out_listBox);
+            this.Controls.Add(this.buttonSTART);
+            this.Name = "Form1";
+            this.Text = "SyncDemo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSTART;
+        private System.Windows.Forms.ListBox out_listBox;
+    }
+}
+
---
+++ Adv3/SyncDemo/SyncDemo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncDemo
+{
+    /// <summary>
+    /// 3.	Create a Mutex object with the name “SyncFileMutex”, starting in the non-signaled state.
+    /// 4.	Open or create a file named “data.txt” in the “c:\temp” folder(create the folder using Windows Explorer or in code if it does not exist).
+    /// 5.	Create a loop that writes 10000 times a string that includes the process identifier
+    /// (look at System.Diagnostics.Process class),
+    /// while providing a WaitOne/ReleaseMutex around the code.
+    /// 6.	Run two instances of the application simultaneously and check the resulting file.
+    /// 7.	Comment out the mutex operations and rerun.
+    /// Make sure you understand the results.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Mutex _myMutex;
+
+        private void out_listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSTART_Click(object sender, EventArgs e)
+        {
+            //3.
+            _myMutex = new Mutex(false, "SyncFileMutex");
+            //4.
+            Directory.CreateDirectory(@"C:\temp");
+            
+            var myFileStream =File.Open(@"C:\temp\data.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            StreamWriter myWriter = new StreamWriter(myFileStream);
+            //5.
+            string text = $"the writing procees id is {Process.GetCurrentProcess().Id}";
+            _myMutex.WaitOne();
+            for (int i = 0; i < 10000; i++)
+            {
+                myWriter.WriteLine(text);
+            }
+            _myMutex.ReleaseMutex();
+            //7.
+
+
+        }
+    }
+}
+
