0299ecbc50432d6f309b5c9e05bc4b36e15add7a
Efrat <efratnor@gmail.com>
2016-08-05 10:19:35 +0300

    from Last week

---
+++ Adv4/DevOpsEx2/DevOpsEx2/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace DevOpsEx2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext txt)
+        {
+            Debug.WriteLine("AssemblyInit");
+        }
+        [ClassInitialize]
+        public static void ClassInit1(TestContext txt)
+        {
+            Debug.WriteLine("ClassInit1");
+        }
+        [TestInitialize]
+        public void TestInint1()
+        {
+            Debug.WriteLine("testInint1");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine($"TestMethod1 in UnitTest1");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Debug.WriteLine($"TestMethod2 in UnitTest1");
+        }
+    }
+}
---
+++ Adv4/DevOpsEx2/DevOpsEx2/UnitTest2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsEx2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [ClassInitialize]
+        public static void ClassInit2(TestContext txt)
+        {
+            Debug.WriteLine("ClassInit2");
+        }
+        [TestInitialize]
+        public void TestInint2()
+        {
+            Debug.WriteLine("testInint2");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine($"TestMethod1 in UnitTest2");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Debug.WriteLine($"TestMethod2 in UnitTest2");
+        }
+    }
+
+}
