5ae4f6ec376b3a703648dca5f85b641f78c6ab6d
Efrat <efratnor@gmail.com>
2016-08-12 09:50:48 +0300

    finished

--- Adv3/PrimesCalculator/PrimesCalculator/Form1.Designer.cs
+++ Adv3/PrimesCalculator/PrimesCalculator/Form1.Designer.cs
@@ -32,13 +32,13 @@ namespace PrimesCalculator
         {
             this.lowBoundBox = new System.Windows.Forms.TextBox();
             this.highBoundBox = new System.Windows.Forms.TextBox();
-            this.primesListBox = new System.Windows.Forms.ListBox();
             this.Calculate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.buttonSTOP = new System.Windows.Forms.Button();
             this.buttonCANCEL = new System.Windows.Forms.Button();
+            this.OutPutLabel = new System.Windows.Forms.Label();
+            this.OutputFileTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lowBoundBox
@@ -47,7 +47,6 @@ namespace PrimesCalculator
             this.lowBoundBox.Name = "lowBoundBox";
             this.lowBoundBox.Size = new System.Drawing.Size(42, 20);
             this.lowBoundBox.TabIndex = 1;
-            this.lowBoundBox.TextChanged += new System.EventHandler(textBox1_TextChanged);
             // 
             // highBoundBox
             // 
@@ -55,15 +54,6 @@ namespace PrimesCalculator
             this.highBoundBox.Name = "highBoundBox";
             this.highBoundBox.Size = new System.Drawing.Size(43, 20);
             this.highBoundBox.TabIndex = 2;
-            this.highBoundBox.TextChanged += new System.EventHandler(textBox2_TextChanged);
-            // 
-            // primesListBox
-            // 
-            this.primesListBox.FormattingEnabled = true;
-            this.primesListBox.Location = new System.Drawing.Point(86, 136);
-            this.primesListBox.Name = "primesListBox";
-            this.primesListBox.Size = new System.Drawing.Size(120, 95);
-            this.primesListBox.TabIndex = 3;
             // 
             // Calculate
             // 
@@ -73,7 +63,7 @@ namespace PrimesCalculator
             this.Calculate.TabIndex = 4;
             this.Calculate.Text = "Calculate";
             this.Calculate.UseVisualStyleBackColor = true;
-            this.Calculate.Click += new System.EventHandler(Calculate_Click);
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
             // 
             // label1
             // 
@@ -93,48 +83,56 @@ namespace PrimesCalculator
             this.label2.TabIndex = 6;
             this.label2.Text = "High Range\'s Bound";
             // 
-            // label3
+            // buttonCANCEL
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(83, 120);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Prime numbers in the range";
-            this.label3.Click += new System.EventHandler(label3_Click);
-            // 
-            // button1
-            // 
-            this.buttonSTOP.Location = new System.Drawing.Point(12, 58);
-            this.buttonSTOP.Name = "button1";
-            this.buttonSTOP.Size = new System.Drawing.Size(58, 23);
-            this.buttonSTOP.TabIndex = 8;
-            this.buttonSTOP.Text = "STOP";
-            this.buttonSTOP.UseVisualStyleBackColor = true;
-            this.buttonSTOP.Click += new System.EventHandler(buttonSTOP_Click);
-            // 
-            // button2
-            // 
-            this.buttonCANCEL.Location = new System.Drawing.Point(12, 87);
-            this.buttonCANCEL.Name = "button2";
+            this.buttonCANCEL.Location = new System.Drawing.Point(313, 72);
+            this.buttonCANCEL.Name = "buttonCANCEL";
             this.buttonCANCEL.Size = new System.Drawing.Size(58, 23);
             this.buttonCANCEL.TabIndex = 9;
             this.buttonCANCEL.Text = "CANCEL";
             this.buttonCANCEL.UseVisualStyleBackColor = true;
             this.buttonCANCEL.Click += new System.EventHandler(this.buttonCANCEL_Click);
             // 
+            // OutPutLabel
+            // 
+            this.OutPutLabel.AccessibleName = "OutPutLabel";
+            this.OutPutLabel.AutoSize = true;
+            this.OutPutLabel.Location = new System.Drawing.Point(64, 150);
+            this.OutPutLabel.Name = "OutPutLabel";
+            this.OutPutLabel.Size = new System.Drawing.Size(69, 13);
+            this.OutPutLabel.TabIndex = 10;
+            this.OutPutLabel.Text = "OutPut Label";
+            // 
+            // OutputFileTextBox
+            // 
+            this.OutputFileTextBox.AccessibleName = "OutputFileTextBox";
+            this.OutputFileTextBox.Location = new System.Drawing.Point(89, 188);
+            this.OutputFileTextBox.Name = "OutputFileTextBox";
+            this.OutputFileTextBox.Size = new System.Drawing.Size(100, 20);
+            this.OutputFileTextBox.TabIndex = 11;
+            this.OutputFileTextBox.Text = "Output File";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Output File";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.buttonCANCEL);
-            this.Controls.Add(this.buttonSTOP);
+            this.ClientSize = new System.Drawing.Size(418, 261);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.OutputFileTextBox);
+            this.Controls.Add(this.OutPutLabel);
+            this.Controls.Add(this.buttonCANCEL);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Calculate);
-            this.Controls.Add(this.primesListBox);
             this.Controls.Add(this.highBoundBox);
             this.Controls.Add(this.lowBoundBox);
             this.Name = "Form1";
@@ -149,13 +147,13 @@ namespace PrimesCalculator
         #endregion
         private System.Windows.Forms.TextBox lowBoundBox;
         private System.Windows.Forms.TextBox highBoundBox;
-        private System.Windows.Forms.ListBox primesListBox;
         private System.Windows.Forms.Button Calculate;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button buttonSTOP;
         private System.Windows.Forms.Button buttonCANCEL;
+        private System.Windows.Forms.Label OutPutLabel;
+        private System.Windows.Forms.TextBox OutputFileTextBox;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Adv3/PrimesCalculator/PrimesCalculator/Form1.cs
+++ Adv3/PrimesCalculator/PrimesCalculator/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -18,50 +19,46 @@ namespace PrimesCalculator
             InitializeComponent();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-        }
-
         private void Form_Load(object sender, EventArgs e)
         {
 
         }
 
-        AutoResetEvent _autoResetEvent;
         CancellationTokenSource _CancellationTokenSource;
 
-        /// <summary>
-        /// 3.
-        /// When the user presses the “Calculate” button,
-        /// spawn a new thread or Task 
-        /// and calculate the prime numbers within the range indicated by the textboxes.
-        /// 
-        /// 4.	
-        /// When done, 
-        /// populate the list box with the numbers. 
-        /// Note that you need to access the list box within the UI thread 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private async void Calculate_Click(object sender, EventArgs e)
         {
             int lowRangeBound, highRangeBound ;
+
             bool isValidInput = GetRangeBounds(out lowRangeBound, out highRangeBound);
+
             if (isValidInput)
             {
-                #region for 7.2
-                _autoResetEvent = new AutoResetEvent(false); //a.
-                _CancellationTokenSource = new CancellationTokenSource(); //b.
+
+                _CancellationTokenSource = new CancellationTokenSource();
+
                 var cacellationToken = _CancellationTokenSource.Token;
-                #endregion
-                List<int> primes = new List<int>();
-                await PrimeNumbersCalculateAsync(lowRangeBound, highRangeBound, primes,cacellationToken);
-                primesListBox.DataSource = primes;
+
+
+               int noOfPrimesInTheRange =await CountPrimesAsync(lowRangeBound, highRangeBound,cacellationToken);
+
+                string outPutText = $"The number of prime-numbers in the range [{lowRangeBound},{highRangeBound}]  is  {noOfPrimesInTheRange}.";
+
+                OutPutLabel.Text = outPutText;
+
+                string outputFilePath = OutputFileTextBox.Text;
+
+                if (Path.IsPathRooted(outputFilePath)&& outputFilePath.EndsWith(".txt"))
+                {
+                    using (StreamWriter writer =new StreamWriter(outputFilePath))
+                    {
+                        writer.Write(outPutText);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("\tInvalid input!\n The entered Output File path wrong, \n\tS We dont write to it.");
+                }
             }
             else
             {
@@ -69,42 +66,38 @@ namespace PrimesCalculator
             }          
         }
         /// <summary>
-        /// calculate the prime numbers within the range
+        /// calculate the nomber of prime numbers within the range
         /// </summary>
         /// <param name="lowRangeBound"></param>
         /// <param name="highRangeBound"></param>
         /// <returns></returns>
-        private async Task PrimeNumbersCalculateAsync(int lowRangeBound, int highRangeBound, List<int> primes,CancellationToken token)
+        private async Task<int> CountPrimesAsync(int lowRangeBound, int highRangeBound,CancellationToken token)
         {
+            int noOfPrimesInTheRange = 0;
+
             PrimearyChecker primeCheck = new PrimearyChecker();
-            await Task.Run(() =>
+
+            return await Task<int>.Run(() =>
             {
                 for (int j = 0; j < (highRangeBound - lowRangeBound + 1); j++)
                 {
                     if (primeCheck.isPrime(lowRangeBound + j))
                     {
-                        primes.Add(lowRangeBound + j);
+                        noOfPrimesInTheRange++;
                     }
                     #region I have added this because i wasn't fast inugh to cacncle or stop the caculateing before it ended.
-                    Thread.Sleep(1000);
+                    //Thread.Sleep(1000);
                     #endregion
-                    #region for 7.2
-                    //a.
-                    bool isSTOPSignald= _autoResetEvent.WaitOne(0);
-                    if(isSTOPSignald)
-                    {
-                        return;
-                    }
-                    //b.
+
                     if (token.IsCancellationRequested)
                     {
-                        return;
+                        return noOfPrimesInTheRange;
                     }
-                    #endregion
                 }
-            }
-            );
+                return noOfPrimesInTheRange;
+            } );
         }
+
         /// <summary>
         /// get the range indicated by the textboxes
         /// </summary>
@@ -113,33 +106,22 @@ namespace PrimesCalculator
         private bool GetRangeBounds(out int lowRangeBound, out int highRangeBound )
         {
             bool isValidInput = int.TryParse((lowBoundBox as TextBox).Text, out lowRangeBound);
+
             isValidInput &= int.TryParse((highBoundBox as TextBox).Text, out highRangeBound);
+
             isValidInput &= lowRangeBound <= highRangeBound;
+
             return isValidInput;
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
         /// <summary>
-        /// 7.2.b.	Use a CancellationToken to signal the cancellation
+        /// Use a CancellationToken to signal the cancellation
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void buttonCANCEL_Click(object sender, EventArgs e)
         {
             _CancellationTokenSource.Cancel();
-        }
-        /// <summary>
-        /// 7.2.a.	Use an Event object to signal the thread to “bail out” early. 
-        /// by call WaitHandle.WaitOne with a timeout of zero to just “check” if the object is signalled.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonSTOP_Click(object sender, EventArgs e)
-        {
-            _autoResetEvent.Set();
         }
     }
 }
